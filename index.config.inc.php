f12c1ecb1aade7a9722351ef9e4a3de302bb1725
LaoLin <hi@laolin.com>
2013-07-20 19:47:12 +0800

    add config hooker

---
+++ index.config.inc.php
@@ -0,0 +1,5 @@
+<?php
+
+$index_config=array();
+
+$index_config['api-wp-include-file']='../../_dev.laolin.com/wordpress/wp-blog-header.php';
--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
 
 $path='linjianping';
+
+if( file_exists('./index.config.inc.php') ) include_once( './index.config.inc.php' );
+
+$c=isset($_GET['c'])?$_GET['c']:'';
+$a=isset($_GET['a'])?$_GET['a']:'';
+
+switch($c) {
+  case 'api':
+    if($c='wp'){//prepare wordpress functions
+      if(file_exists($index_config['api-wp-include-file']))
+        include_once($index_config['api-wp-include-file']);
+    }
+    break;
+}
 /*
 if(!isset($_GET['appname']))$_GET['appname']='';
 
